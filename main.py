bee0c39a74fd09ee03537ef3934f2d17e0aec798
mohitparekh7 <54593876+mohitparekh7@users.noreply.github.com>
2020-10-11 19:12:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,70 @@
-import sympy as sp
+# import sympy as sp
 from tkinter import *
 
-t = Tk()
-t.title('Cryptography')
-t.resizable(width = False  , height = True)
 
 def rsaExecute():
     print("In RSA")
-    #Public Key
-    p=sp.randprime(30,100)
-    q=sp.randprime(30,100)
-    #First number in Publc Key
-    n=p*q
-    phi=(p-1)*(q-1)
 
-    #Private Key
-    for e in range(2,phi):
-        for i in range(1, e + 1):
-            if ((phi % i == 0) and (e % i == 0)):
-                gcd = i
-        if gcd== 1:
-            break
+def runPFEncryption():
+    print("Encrypting Playfair..........")
 
-    #Second number in Public Key
-    d=0
-    for k in range(1,10):
-        x = 1 + k*phi
-        if x % e == 0:
-            d = int(x/e)
-            break
+def encryptPlayfair():
+    global encryptPlayfairWindow
+    encryptPlayfairWindow = Tk()
+    encryptPlayfairWindow.title('Encrypt')
 
-    #Input
-    data=int(input("Enter number to encrypt:"))
+    l1 = Label(encryptPlayfairWindow, text='Key:')
+    l1.grid(row=1,column=1)
+    l2 = Label(encryptPlayfairWindow, text='Message:')
+    l2.grid(row = 2, column = 1)
 
-    #Encryption
-    encrypted=(data**e)%n
-    print("\nEncrypted Message:",encrypted)
+    key_text = StringVar()
+    e1 = Entry(encryptPlayfairWindow, textvariable=key_text)
+    # e1 = Entry(encryptPlayfairWindow)
+    e1.grid(row=1,column=2)
+    key = key_text.get()
 
-    #Decryption
-    decrypted=int((encrypted**d)%n)
-    print("\nDecrypted Message:",decrypted)
+    message_text = StringVar()
+    e2 = Entry(encryptPlayfairWindow, textvariable=message_text)
+    e2.grid(row=2,column=2)
 
-    print("\nDetails:")
-    print("Public Key:",n,"&",d)
-    print("Private Key:",e)
-    print("Phi:",phi,"\nP:",p,"\nQ:",q,"\nk:",k)
+    b1 = Button(encryptPlayfairWindow,text='Submit',width=12,pady=5,command = runPFEncryption)
+    b1.grid(row=5,column=5)
+
+    encryptPlayfairWindow.mainloop()
+
 
 def playfairExecute():
     print("In PLayfair")
+    t.destroy()
+    global playfairWindow
+    playfairWindow = Tk()
+    playfairWindow.title('Playfair')
 
-rsa_button = Button(text = 'RSA',command= rsaExecute, width = 50 , height = 5 )
-playfair_button = Button(text = 'PlayFair',command = playfairExecute, width = 50 , height = 5)
-rsa_button.grid(column = 1 , row =  1)
-playfair_button.grid(column = 1, row = 2)
+    encryptionButton = Button(text = 'Encrypt',command= encryptPlayfair, width = 50 , height = 5 )
+    decryptionButton = Button(text = 'Decrypt',command= backToStart, width = 50 , height = 5 )
+    backButton = Button(text = 'Back',command= backToStart, width = 50 , height = 5 )
+    encryptionButton.grid(column = 1 , row =  1)
+    decryptionButton.grid(column = 1 , row =  2)
+    backButton.grid(column = 1 , row =  3)
 
-t.mainloop()
+    playfairWindow.mainloop()
+
+def backToStart(): 
+    playfairWindow.destroy()
+    start()
+
+def start():
+    global t
+    t = Tk()
+    t.title('Cryptography')
+    t.resizable(width = False  , height = True)
+
+    rsa_button = Button(text = 'RSA',command= rsaExecute, width = 50 , height = 5 )
+    playfair_button = Button(text = 'PlayFair',command = playfairExecute, width = 50 , height = 5)
+    rsa_button.grid(column = 1 , row =  1)
+    playfair_button.grid(column = 1, row = 2)
+
+    t.mainloop()
+
+start()
